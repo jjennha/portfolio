dfe53b51d824f18f8189d9c612123ea59c774795
Jenny <jha@iastate.edu>
2020-04-12 15:00:00 -0500

    typescript

---
+++ portfolio/src/components/Cover.tsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+type Props = {
+
+}
+export default class Cover extends Component<Props>{
+    constructor(props: Props){
+        super(props);
+    }
+    render(){
+        return (<div>Cover</div>);
+    }
+}
