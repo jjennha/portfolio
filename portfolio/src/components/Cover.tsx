a5be8683b870ef08b887a575e76ac17b5424927c
Jenny <jha@iastate.edu>
2020-04-12 18:02:27 -0500

    cover

--- portfolio/src/components/Cover.tsx
+++ portfolio/src/components/Cover.tsx
@@ -1,13 +1,38 @@
-import React, {Component} from 'react';
-
+import React, { Component } from 'react';
+import profile from "https://i.imgur.com/K2zj5sk.png";
 type Props = {
 
 }
 export default class Cover extends Component<Props>{
-    constructor(props: Props){
+    constructor(props: Props) {
         super(props);
     }
-    render(){
-        return (<div>Cover</div>);
+    getProfile() {
+        const backgroundStyles = {
+            backgroundImage: `url(${
+                profile
+                })`,
+            backgroundSize: '15rem'
+        }
+        return backgroundStyles;
+    }
+    render() {
+        return (
+            <div className="cover-container">
+                <div className="cover-content">
+                <div className="cover-photo">
+                    <div className="profile" style={this.getProfile()}></div>
+                    </div>
+                    <div className="cover-text">
+                        
+                        <h1 className="text-intro">Hello World</h1>
+                        <h1 className="text-me">I'm Jenny</h1>
+
+                    </div>
+                    
+                    
+                </div>
+            </div>
+        );
     }
 }
